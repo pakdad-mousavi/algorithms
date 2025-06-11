589a491c3d633de8365c8c15726269c4a5a95136
Pakdad Mousavi <pakdad.mousavi@gmail.com>
2025-06-12 00:55:42 +0800

    Initial commit.

---
+++ src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup>
+import { NButton } from 'naive-ui'
+</script>
+
+<template>
+  <main>
+    <n-button>naive-ui</n-button>
+    <h1>This is the home paged</h1>
+  </main>
+</template>
