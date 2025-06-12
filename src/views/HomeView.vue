7946b86a8a7d49b15ddd40da102b26910a675be1
Pakdad Mousavi <pakdad.mousavi@gmail.com>
2025-06-12 19:12:04 +0800

    Remove naive ui from the project.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,8 @@
 <script setup>
-import { NButton } from 'naive-ui'
 </script>
 
 <template>
   <main>
-    <n-button>naive-ui</n-button>
-    <h1>This is the home paged</h1>
+
   </main>
 </template>
