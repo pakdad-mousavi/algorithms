8809d378f69c02b219274c7a9604a2bddf2ed32c
Pakdad Mousavi <pakdad.mousavi@gmail.com>
2025-06-13 19:44:59 +0800

    Create the sidebar, displaying the group tree.

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,112 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-</script>
-
 <template>
-  <header>
-    <div>
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
+  <header></header>
+  <main class="flex p-4">
+    <nav class="flex flex-col bg-neutral-800/55 rounded-l-xl border-[1px] border-neutral-800">
+      <div class="flex p-4 font-bold border-b-[1px] border-neutral-800 mb-4">
+        ALGORITHMS
+      </div>
+      <div class="p-4 text-sm">
+        <GroupTree v-for="(page, index) in pages" :key="index" :group="page" :icons="pageIcons" :path="[]"
+          :showTreeLines="false" class="!ml-0 mb-2" />
+      </div>
+    </nav>
 
-  <RouterView />
+    <div>
+      <RouterView />
+    </div>
+  </main>
 </template>
 
-<style scoped></style>
+<script setup>
+import { RouterView } from "vue-router";
+import GroupTree from "./components/GroupTree.vue";
+import { DeveloperBoard24Regular, LockClosed24Regular, Storage24Regular, Database20Regular, ArrowCollapseAll20Filled } from '@vicons/fluent';
+import { VmdkDisk } from "@vicons/carbon";
+
+const pageIcons = {
+  DeveloperBoard24Regular,
+  LockClosed24Regular,
+  Storage24Regular,
+  Database20Regular,
+  ArrowCollapseAll20Filled,
+  VmdkDisk
+};
+
+const pages = [
+  {
+    name: "CPU Scheduling",
+    icon: "DeveloperBoard24Regular",
+    subgroups: [
+      {
+        name: "Pre-emptive Algorithms",
+        subgroups: null,
+        algorithms: [
+          "Round Robin",
+        ],
+      },
+      {
+        name: "Non Pre-emptive Algorithms",
+        subgroups: null,
+        algorithms: [
+          "First Come First Serve",
+          "Priority",
+          "Shortest Job First",
+        ],
+      }
+    ],
+  },
+  {
+    name: "Deadlock Management",
+    icon: "LockClosed24Regular",
+    subgroups: null,
+    algorithms: [
+      "Bankers Algorithm",
+    ]
+  },
+  {
+    name: "Memory Management",
+    icon: "Database20Regular",
+    subgroups: [
+      {
+        name: "External Fragmentation",
+        subgroups: null,
+        algorithms: [
+          "First Fit",
+          "Best Fit",
+          "Worst Fit"
+        ],
+      }
+    ],
+  },
+  {
+    name: "Virtual Memory Management",
+    icon: "Storage24Regular",
+    subgroups: [
+      {
+        name: "Page Replacements",
+        subgroups: null,
+        algorithms: [
+          "First In First Out",
+          "Optimal Replacement",
+          "Least Recently Used"
+        ]
+      }
+    ]
+  },
+  {
+    name: "Disk Scheduling",
+    icon: "VmdkDisk",
+    subgroups: [
+
+    ],
+    algorithms: [
+      "First Come First Serve",
+      "Shortest Seek Time First",
+      "SCAN",
+      "CSCAN",
+      "LOOK",
+      "CLOOK"
+    ]
+  }
+];
+</script>
---
+++ src/components/GroupTree.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="ml-3">
+    <div class="flex items-center hover:bg-neutral-800 py-1.5 duration-100 px-1 rounded-md border-neutral-700"
+      @click="isMinimized = !isMinimized"
+      :class="{ 'is-minimized': isMinimized, 'border-l-[1px] rounded-none !rounded-r-md pl-2': showTreeLines }">
+      <Icon v-if="group.icon" tag="span" size="20" class="mr-1">
+        <component :is="icons[group.icon]"></component>
+      </Icon>
+      <div class="flex-1 mr-6">{{ group.name }}</div>
+      <Icon tag="span" size="16" class="duration-300 rotate-90 chevron">
+        <ChevronRight24Filled />
+      </Icon>
+    </div>
+
+    <!-- Render subgroups recursively if they exist -->
+    <div v-if="group.subgroups && group.subgroups.length" :class="{ 'mb-6': !isMinimized }">
+      <GroupTree v-for="(subgroup, index) in group.subgroups" :key="index" :group="subgroup"
+        :path="[...path, group.name]" :showTreeLines="true" />
+    </div>
+
+    <!-- Render algorithms if they exist -->
+    <ul v-if="group.algorithms && group.algorithms.length">
+      <li v-for="(algorithm, index) in group.algorithms" :key="index"
+        :class="{ 'mb-6': !isMinimized & index === group.algorithms.length - 1 }"
+        class="ml-3 border-l-[1px] border-neutral-700 hover:bg-neutral-800 rounded-r-md duration-100">
+        <RouterLink :to="buildPath([...path, group.name, algorithm])" class="block w-full h-full p-2 py-2">
+          {{ algorithm }}
+        </RouterLink>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import { RouterLink } from 'vue-router'
+import { Icon } from '@vicons/utils';
+import { ChevronRight24Filled } from '@vicons/fluent';
+
+defineProps({
+  group: Object,
+  icons: Object,
+  path: {
+    type: Array,
+    default: () => [],
+  },
+  showTreeLines: Boolean,
+
+});
+
+const isMinimized = ref(true);
+
+// Utility to construct URL-safe path
+const buildPath = (segments) => {
+  return '/' + segments.map(s => s.toLowerCase().replace(/ /g, "-")).join('/')
+};
+</script>
