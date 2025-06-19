ae0093fc71efc4289820a9cfbaaac7cad2e31ff4
Pakdad Mousavi <pakdad.mousavi@gmail.com>
2025-06-19 18:33:12 +0800

    Add slight rotation to the moon icon.

--- src/components/EmptySpace.vue
+++ src/components/EmptySpace.vue
@@ -4,7 +4,7 @@
     <div class="relative flex items-center justify-center w-full h-full">
       <Icon class="absolute -translate-x-1/2 top-1/3 -translate-y-1/3 text-zinc-700 text-9xl left-1/2 animate-bounce"
         tag="span">
-        <Moon></Moon>
+        <Moon class="-rotate-10"></Moon>
       </Icon>
       <div class="z-10 w-3/4 pt-4 space-y-2 text-center border-t-2 translate-y-2/3 border-zinc-700 h-1/3">
         <slot></slot>
