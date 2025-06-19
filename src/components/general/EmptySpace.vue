ddb2cd1c00255d19eb2bbf89b1e22c116ae729a8
Pakdad Mousavi <pakdad.mousavi@gmail.com>
2025-06-19 23:41:03 +0800

    Simplify the style of the EmptySpace component.

--- src/components/general/EmptySpace.vue
+++ src/components/general/EmptySpace.vue
@@ -1,12 +1,11 @@
 <template>
   <div
     class="border rounded-md border-zinc-700 h-80 bg-[image:repeating-radial-gradient(var(--color-zinc-800)_0,var(--color-zinc-800)_1px,_transparent_1px,_transparent_100%)] bg-[size:20px_20px] bg-fixed">
-    <div class="relative flex items-center justify-center w-full h-full">
-      <Icon class="absolute -translate-x-1/2 top-1/3 -translate-y-1/3 text-zinc-700 text-9xl left-1/2 animate-bounce"
-        tag="span">
-        <Moon class="-rotate-10"></Moon>
+    <div class="relative flex flex-col items-center justify-center w-full h-full">
+      <Icon class="text-zinc-700 text-9xl" tag="span">
+        <CogSharp></CogSharp>
       </Icon>
-      <div class="z-10 w-3/4 pt-4 space-y-2 text-center border-t-2 translate-y-2/3 border-zinc-700 h-1/3">
+      <div class="z-10 space-y-2 text-center">
         <slot></slot>
       </div>
     </div>
@@ -14,6 +13,6 @@
 </template>
 
 <script setup>
-import { Moon } from '@vicons/tabler';
+import { CogSharp } from '@vicons/ionicons5';
 import { Icon } from '@vicons/utils';
 </script>
