81a5b141643551d4e0ca597c8dcbd72687d109f9
Pakdad Mousavi <pakdad.mousavi@gmail.com>
2025-07-01 20:50:58 +0800

    Create the homepage.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,32 @@
+<template>
+  <div>
+    <div class="my-8">
+      <div class="relative flex justify-center">
+        <div
+          class="absolute top-0 bottom-0 bg-emerald-400/50 aspect-square -z-10 blur-3xl opacity-30 animate-[pulse_5s_3s_infinite]">
+        </div>
+        <Logo :animated="true"></Logo>
+      </div>
+      <div class="mt-6 space-y-2 text-center">
+        <h1 class="text-4xl font-bold opacity-0 title">Learning Algorithms</h1>
+        <p class="text-lg opacity-0 subtitle">A Visual Guide to Understanding <span class="text-main">Algorithms</span>
+        </p>
+      </div>
+    </div>
+    <div class="flex flex-col items-center gap-y-4 opacity-0 content">
+      <p class="text-center lg:w-4/5">
+        A minimalist platform for learning algorithms visually with step-by-step explanations, clean interfaces, and
+        intuitive breakdowns to help you truly understand how algorithms work.
+      </p>
+      <div>
+        <div href="/cpu-scheduling/round-robin" class="btn">Start Learning</div>
+      </div>
+    </div>
+  </div>
+</template>
+
 <script setup>
+import Logo from '@/components/Logo.vue';
 </script>
 
-<template>
-  <main>
-
-  </main>
-</template>
+<style scoped></style>
