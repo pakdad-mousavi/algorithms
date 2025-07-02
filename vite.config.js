7f110759ffb9af3d32b2e45aa099a06e0370c1ec
Pakdad Mousavi <pakdad.mousavi@gmail.com>
2025-07-02 11:56:39 +0800

    Add base to vite config.

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
+  base: "/algorithms/",
   plugins: [vue(), tailwindcss()],
   resolve: {
     alias: {
