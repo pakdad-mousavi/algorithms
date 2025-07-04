ab6f6777b5168ef09b23fc8c2c92deb08f83f7aa
Pakdad Mousavi <pakdad.mousavi@gmail.com>
2025-07-04 21:17:14 +0800

    Increase brightness of the TabHeader backdrop on hover.

--- src/components/TabHeader.vue
+++ src/components/TabHeader.vue
@@ -3,15 +3,15 @@
     <h1 class="flex-1 text-2xl font-semibold">
       {{ route.meta.name }}
     </h1>
-    <div class="relative">
+    <div class="relative group max-w-max">
       <ul
-        class="flex overflow-hidden text-sm font-medium duration-75 border rounded-md max-w-max border-neutral-700 hover:border-main">
+        class="flex overflow-hidden text-sm font-medium duration-75 border rounded-md border-neutral-700 hover:border-main">
         <li class="px-4 py-1 duration-75 cursor-pointer" v-for="tab in tabs" @click="emit('on-tab-change', tab);"
           :class="{ 'bg-main text-neutral-700': activeTab === tab }">
           {{ tab }}
         </li>
       </ul>
-      <div class="absolute inset-0 rounded-md bg-main/20 -z-10 blur-2xl"></div>
+      <div class="absolute inset-0 duration-300 rounded-md bg-main/20 group-hover:bg-main/30 -z-10 blur-2xl"></div>
     </div>
   </div>
   <hr class="mb-4 border-neutral-800">
