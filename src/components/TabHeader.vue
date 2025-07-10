de80c361d41b228649e7b40eac31cad3b731adfe
Pakdad Mousavi <pakdad.mousavi@gmail.com>
2025-07-10 16:41:04 +0800

    Update the style of the tab header.

--- src/components/TabHeader.vue
+++ src/components/TabHeader.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="flex flex-col mb-4 sm:flex-row gap-y-4">
-    <h1 class="flex-1 text-2xl font-semibold">
+  <div class="flex flex-col mb-4 sm:flex-row gap-y-4 items-center gap-x-2">
+    <h1 class="flex-1 text-2xl font-semibold text-center sm:text-left">
       {{ route.meta.name }}
     </h1>
-    <div class="relative group max-w-max">
+    <div class="relative group sm:max-w-max w-full max-w-sm">
       <ul
-        class="flex overflow-hidden text-sm font-medium duration-75 border rounded-md border-neutral-700 hover:border-main">
-        <li class="px-4 py-1 duration-75 cursor-pointer" v-for="tab in tabs" @click="emit('on-tab-change', tab);"
+        class="flex flex-col sm:flex-row overflow-hidden text-sm font-medium duration-75 border rounded-md border-neutral-700 hover:border-main">
+        <li class="px-4 py-1.5 sm:py-1 duration-75 cursor-pointer text-center" v-for="tab in tabs" @click="emit('on-tab-change', tab);"
           :class="{ 'bg-main text-neutral-700': activeTab === tab }">
           {{ tab }}
         </li>
