0af77cf2f8dddbd11fe83931ff54d20a58467ac1
Pakdad Mousavi <pakdad.mousavi@gmail.com>
2025-07-15 14:01:33 +0800

    Add the LRU and optimal page replacement strategies.

--- src/composables/virtual-memory-management.js
+++ src/composables/virtual-memory-management.js
@@ -3,40 +3,63 @@ class LruCache {
     this.size = size;
     this.cache = new Array(size).fill(null);
   }
-
   update = (val) => {
+    if (this.cache.includes(val)) {
+      const pos = this.cache.indexOf(val);
+      this.cache.splice(pos, 1);
+    } else {
+      this.cache.pop();
+    }
     this.cache.unshift(val);
-    this.cache.pop();
   };
-
   getLruItem = () => this.cache[this.size - 1];
 }
 
 const replacementStrategies = {
-  fifo: (_) => {
+  fifo: () => {
     const queue = [];
     return {
-      update: (item) => queue.push(item),
+      update: (item) => {
+        if (!queue.includes(item)) queue.push(item);
+      },
       getVictimPage: () => queue.shift(),
     };
   },
   lru: ({ frameCount }) => {
-    return { update, getVictimPage };
+    const cache = new LruCache(frameCount);
+    return {
+      update: cache.update,
+      getVictimPage: cache.getLruItem,
+    };
   },
-  optimal: ({ refStr }) => {
-    return { update, getVictimPage };
+  optimal: ({ frames, refStr }) => {
+    let currentIdx = 0;
+    const getVictimPage = () => {
+      const nextUse = frames.map((f) => refStr.indexOf(f, currentIdx));
+      const farthest = Math.max(...nextUse);
+      // Pick the first non-reoccurring page or the farthest
+      const victimIdx = nextUse.includes(-1) ? nextUse.indexOf(-1) : nextUse.indexOf(farthest);
+      return frames[victimIdx];
+    };
+    return {
+      update: () => {
+        currentIdx++;
+      },
+      getVictimPage,
+    };
   },
 };
 
 export const runPageReplacementAlgorithm = (refStr, frameCount, replacementStrategy) => {
-  const ctx = { refStr, frameCount };
-  const strategy = replacementStrategies[replacementStrategy];
-  const { update, getVictimPage } = strategy(ctx);
-
   const log = [];
   let totalPageFaults = 0;
   const frames = new Array(frameCount).fill(null);
 
+  const ctx = { refStr, frames, frameCount };
+  const strategy = replacementStrategies[replacementStrategy];
+  const { update, getVictimPage } = strategy(ctx);
+
+  // Run the respective page replacement algorithm
   for (const pageId of refStr) {
     let isPageFault = false;
     let victimPageFrameIdx;
@@ -49,10 +72,10 @@ export const runPageReplacementAlgorithm = (refStr, frameCount, replacementStrat
         victimPageFrameIdx = frames.findIndex((f) => f === victimPage);
         frames.splice(victimPageFrameIdx, 1, pageId);
       }
-      update(pageId);
       totalPageFaults++;
       isPageFault = true;
     }
+    update(pageId);
     log.push({ frames: frames.slice(), isPageFault, victimPageFrameIdx });
   }
 
