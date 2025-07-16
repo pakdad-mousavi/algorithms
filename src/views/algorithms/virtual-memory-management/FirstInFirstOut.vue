2571fdd82e4e2f9cec2d0460af96ab74d7aac1ed
Pakdad Mousavi <pakdad.mousavi@gmail.com>
2025-07-16 13:53:33 +0800

    Ensure rows can only be deleted if there is more than one present.

--- src/views/algorithms/virtual-memory-management/FirstInFirstOut.vue
+++ src/views/algorithms/virtual-memory-management/FirstInFirstOut.vue
@@ -114,7 +114,7 @@
                   <td class="w-20 mx-auto text-center">
                     <div
                       class="flex items-center justify-center duration-100 border border-transparent rounded-md cursor-pointer bg-zinc-700 aspect-square w-7 group hover:border-rose-600 active:translate-y-1"
-                      @click="removeRow(index)">
+                      @click="removeRow(index)" v-if="referenceStr.length > 1">
                       <Icon class="text-rose-500" tag="span" size="20px">
                         <Trash></Trash>
                       </Icon>
