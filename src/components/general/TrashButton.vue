768a8904922aa7a25a157802e54e857909c64ae6
Pakdad Mousavi <pakdad.mousavi@gmail.com>
2025-07-20 11:01:57 +0800

    Remove translate Y when active to ensure button click is registered.

--- src/components/general/TrashButton.vue
+++ src/components/general/TrashButton.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="flex items-center justify-center duration-100 border border-transparent rounded-md cursor-pointer bg-zinc-700 aspect-square w-7 group hover:border-rose-600 active:translate-y-1">
+    class="flex items-center justify-center w-7 h-7 duration-100 border border-transparent rounded-md cursor-pointer bg-zinc-700 group hover:border-rose-600 active:bg-zinc-800">
     <Icon class="text-rose-500" tag="span" size="20px">
       <Trash></Trash>
     </Icon>
