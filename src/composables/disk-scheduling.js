ee378dc599f919d97d150568737bb921b54b9a2b
Pakdad Mousavi <pakdad.mousavi@gmail.com>
2025-07-22 17:47:39 +0800

    Share a context with the scheduling strategies and create the SCAN strategy.

--- src/composables/disk-scheduling.js
+++ src/composables/disk-scheduling.js
@@ -1,8 +1,8 @@
 const schedulingStrategies = {
-  fcfs: (headPosition, diskRequests) => {
+  fcfs: ({ headPosition, diskRequests }) => {
     return [headPosition, ...diskRequests];
   },
-  sstf: (headPosition, diskRequests) => {
+  sstf: ({ headPosition, diskRequests }) => {
     const pending = [...diskRequests]; // clone to avoid mutating original
     const sequence = [headPosition];
     let current = headPosition;
@@ -27,12 +27,49 @@ const schedulingStrategies = {
 
     return sequence;
   },
+  scan: ({ headPosition, diskRequests, headDirection }) => {
+    const MIN_TRACK = 0;
+    const MAX_TRACK = 199;
+    const pending = [headPosition, ...diskRequests];
+
+    // Include min and max tracks if needed
+    if (pending.indexOf(MIN_TRACK) === -1) {
+      pending.push(MIN_TRACK);
+    }
+    if (pending.indexOf(MAX_TRACK) === -1) {
+      pending.push(MAX_TRACK);
+    }
+
+    const orderedRequests = pending.sort((a, b) => a - b);
+    const headIndex = orderedRequests.indexOf(headPosition);
+
+    let firstSegment;
+    if (headDirection === "left") {
+      // Take requests from 0 to head, then reverse
+      firstSegment = orderedRequests.splice(0, headIndex + 1).reverse();
+    } else {
+      // Take requests from head to end, then reverse remaining requests
+      firstSegment = orderedRequests.splice(headIndex, orderedRequests.length);
+      orderedRequests.reverse();
+    }
+
+    const sequence = firstSegment.concat(orderedRequests);
+    console.log(sequence);
+
+    return sequence;
+  },
 };
 
 // Rename this to whatever specific algorithm (e.g: runSortingAlgorithm)
-export const runDiskSchedulingAlgorithm = (headPosition, diskRequests, schedulingStrategy) => {
+export const runDiskSchedulingAlgorithm = (
+  headPosition,
+  diskRequests,
+  schedulingStrategy,
+  headDirection = "left" // 'left' or 'right'
+) => {
+  const ctx = { headPosition, diskRequests, headDirection };
   const strategy = schedulingStrategies[schedulingStrategy];
-  const orderedRequests = strategy(headPosition, diskRequests);
+  const orderedRequests = strategy(ctx);
   let totalSeekTime = 0;
 
   // The first item is not needed, skip it
