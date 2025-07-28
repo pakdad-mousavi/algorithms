e58bac2177f9e9429790b9319e82be105fd51cdd
Pakdad Mousavi <pakdad.mousavi@gmail.com>
2025-07-29 00:52:18 +0800

    Remove unnecessary attribution from the footer.

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,15 +1,12 @@
 <template>
   <footer class="w-full py-4 text-sm border-t border-neutral-800">
-    <div class="flex flex-col items-center justify-center w-full h-full text-zinc-500">
+    <div class="flex items-center justify-center w-full h-full text-zinc-500">
       <p class="flex items-center gap-x-2">
         Built with
         <Icon>
           <Heart></Heart>
         </Icon>
         by Pakdad Mousavi.
-      </p>
-      <p>
-        All figures made using <a href="http://app.napkin.ai" target="_blank">Napkin AI</a>.
       </p>
     </div>
   </footer>
